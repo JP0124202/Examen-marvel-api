87c3d687e3ac6001e34ead4209dafaf6015389c5
JP0124202 <jairjoaopacheco@gmail.com>
2026-08-16 16:12:22 -0500

    primer commit: proyecto limpio

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet, Navigate, useLocation } from 'react-router-dom'
-
-export default function ProtectedRoute() {
-  const token = localStorage.getItem('token')
-  const location = useLocation()
-  if (!token) return <Navigate to="/login" state={{ from: location }} replace />
-  return <Outlet />
-}
